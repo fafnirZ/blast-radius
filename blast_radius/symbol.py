1dcc35a84c584749223ab986751b2458d914ee04
Jacky <gokurohono@gmail.com>
2025-11-15 21:39:35 +1100

    feat: updates to symbol class

--- blast_radius/symbol.py
+++ blast_radius/symbol.py
@@ -1,18 +1,39 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
-class Symbol:
+class Symbol(ABC):
+    """A symbol must be globally unique so must include a path to the file.
+    
+    /path/to/file.py:ClassName
+    """
     value: str
 
-class ClassSymbol(Symbol):
     def __post_init__(self):
-        assert len(self.value.split(".")) == 1
+        assert self.value.split(":") == 2
+        path_to_file, symbol = self.value.split(":")
+        assert Path(path_to_file).is_file()
+        self.symbol_validation()
+
+    @abstractmethod 
+    def symbol_validation(self):
+        raise NotImplementedError
+
+
+class ClassSymbol(Symbol):
+    def symbol_validation(self):
+        _, symbol = self.value.split(":")
+        assert len(symbol.split(".")) == 1
+
 
 class MethodSymbol(Symbol):
-    def __post_init__(self):
-        assert len(self.value.split(".")) == 2
+    def symbol_validation(self):
+        _, symbol = self.value.split(":")
+        assert len(symbol.split(".")) == 2
 
 class FunctionSymbol(Symbol):
-    def __post_init__(self):
-        assert len(self.value.split(".")) == 1
+    def symbol_validation(self):
+        _, symbol = self.value.split(":")
+        assert len(symbol.split(".")) == 1
