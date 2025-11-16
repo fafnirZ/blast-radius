a3c7a238bf949e5417ff578e4211677140c27d0b
Jacky <gokurohono@gmail.com>
2025-11-16 15:29:42 +1100

    WIP: need a better grand unifying theory of how I should proceed. current code is good POC of functionality

---
+++ blast_radius/operations.py
@@ -0,0 +1,13 @@
+# This aims to list out all permutations of operations.
+
+
+class Operation:
+    pass
+
+class VariableAssignmentOfClass(Operation):
+    pass
+
+class VariableAssignmentOfClassAttribute(Operation):
+    pass
+
+
---
+++ blast_radius/parsers/symbol_defn_tracker.py
@@ -0,0 +1,6 @@
+
+# establish the blast radius of the symbol within the definition.
+#
+# e.g.
+#   symbol: Class.Attribute -> [Class, VariablesAssignedToClass]
+#   symbol: Class.Method -> [Class, VariablesAssignedToClass]
